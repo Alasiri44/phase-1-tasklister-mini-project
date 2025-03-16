08939cba26239389fcc2ff8067c3703f2f1fcea7
alasiri <austinalasiri44@gmail.com>
2025-03-16 16:26:41 +0300

    Finished the challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ document.querySelector('#create-task-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const task = document.getElementById('new-task-description').value;
+
+    const li = document.createElement("li");
+    li.innerHTML = `${task} `;
+
+    const button = document.createElement('button');
+    button.innerHTML = 'X';
+    li.appendChild(button);
+
+    const ul = document.getElementById('tasks');
+    ul.append(li);
+
+    button.addEventListener('click', function(e) {
+      e.target.parentNode.remove();
+  });
+
+  });
+
+  
 });
+
+console.log('How do you live?');
